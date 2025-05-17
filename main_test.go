b92dd779fe9101458757eb255466bf99e2018e9d
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-05-17 09:27:49 +0200

    test: validate http method

---
+++ main_test.go
@@ -0,0 +1,57 @@
+package caddy_site_deployer
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+)
+
+func newTestSiteDeployer() *SiteDeployer {
+	tmp, err := os.MkdirTemp("", "caddy-site-deployer-test-")
+	if err != nil {
+		log.Fatal(tmp)
+	}
+	return &SiteDeployer{
+		Root:      tmp,
+		MaxSizeMB: 1,
+		maxSizeB:  1024 * 1024,
+		logger:    zap.NewNop(),
+	}
+}
+
+type MockHandler struct {
+}
+
+func (m *MockHandler) ServeHTTP(http.ResponseWriter, *http.Request) error { return nil }
+
+func TestOnlyPUTAllowed(t *testing.T) {
+	var tests = []string{
+		http.MethodGet,
+		http.MethodDelete,
+		http.MethodHead,
+		http.MethodPatch,
+		http.MethodPost,
+	}
+	deployer := newTestSiteDeployer()
+
+	for _, method := range tests {
+		t.Run(method, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest(method, "/", nil)
+
+			err := deployer.ServeHTTP(w, r, &MockHandler{})
+			errHandler, ok := err.(caddyhttp.HandlerError)
+
+			assert.NotNil(t, err)
+			assert.True(t, ok)
+			assert.Equal(t, http.StatusMethodNotAllowed, errHandler.StatusCode)
+			assert.Equal(t, "Only PUT method is allowed", w.Body.String())
+		})
+	}
+}
